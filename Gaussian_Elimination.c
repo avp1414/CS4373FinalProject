bf498678658a5cb084a992cbbb2b30a85c24855b
asr1875 <asr1875@utulsa.edu>
2025-04-23 10:51:44 -0500

    Parallelized Gaussian elimination

--- Gaussian_Elimination.c
+++ Gaussian_Elimination.c
@@ -1,19 +1,31 @@
 #include <math.h>
+#include <stdlib.h>
+
+#include <omp.h>
+
 #include "Gaussian_Elimination.h"
 
 /*
 Use Gaussian elimination to triangularize square matrix while preserving determinant
 */
 void triangularize(double* matrix, int n){
+    double* factors = malloc(n*sizeof(double));
     for(int i = 0; i < n-1; i++){
         double piv = matrix[i*n+i];
+
+        #pragma omp parallel for
         for(int j = i+1; j < n; j++){
-            double factor = matrix[j*n+i]/piv;
-            for(int k = i; k < n; k++){ // Parallelize this loop
-                matrix[j*n+k] -= factor*matrix[i*n+k];
+            factors[j] = matrix[j*n+i]/piv;
+        }
+
+        #pragma omp parallel for collapse(2)
+        for(int j = i+1; j < n; j++){
+            for(int k = i; k < n; k++){
+                matrix[j*n+k] -= factors[j]*matrix[i*n+k];
             }
         }
     }
+    free(factors);
 }
 
 /*
@@ -28,7 +40,7 @@ double getDeterminant(double* matrix, int n){
 }
 
 /*
-Calculates log_10 of abs of determinant of a triangular matrix
+Calculates log_10 of absolute value of determinant of a triangular matrix
 */
 double getLogAbsDeterminant(double* matrix, int n){
     double det = 0;
